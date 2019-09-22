64f00c5944a8d40a7fa59b294d5cfcb21605415f
LadyCookie <bouzerea@etu.insa-toulouse.fr>
2019-09-22 17:02:14 -0400

    adding private attributes to ThunderChat headers

--- lib_client/main/mylib/include/ThunderChatClient.hpp
+++ lib_client/main/mylib/include/ThunderChatClient.hpp
@@ -1,6 +1,29 @@
 #include <string>
 #include "Message.hpp"
 
+#ifdef _WIN32
+
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+
+#else
+
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <errno.h>
+
+using SOCKET = inet
+#define SD_BOTH SHUT_RDWR
+#define closesocket(s) close(s)
+
+#endif
+
 class ThunderChatClient{
     public:
     ThunderChatClient(std::string serverName,std::string playerId,int team);
@@ -10,4 +33,10 @@ class ThunderChatClient{
     void OnDisconnet();
     void SendToParty(const std::string& msg);
     void SendToTeam(const std::string& msg);
+
+    private:
+    std::string serverName;
+    std::string playerId;
+    int team;
+    SOCKET sock;
 };
--- lib_server/main/mylib/include/ThunderChatServer.hpp
+++ lib_server/main/mylib/include/ThunderChatServer.hpp
@@ -1,5 +1,29 @@
 #include "Message.hpp"
 #include <string>
+#include<vector>
+
+#ifdef _WIN32
+
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+
+#else
+
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <errno.h>
+
+using SOCKET = inet
+#define SD_BOTH SHUT_RDWR
+#define closesocket(s) close(s)
+
+#endif
 
 class ThunderChatServer{
     public:
@@ -9,5 +33,5 @@ class ThunderChatServer{
     void Stop();
 
     private:
-    //sockets
+    std::vector<SOCKET> all_sockets;
 };
