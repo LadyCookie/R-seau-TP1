a55ee9ab7dfa7db8d6e74bdf52a4cdf47652b201
Mehdi R <rame3116@gmail.com>
2019-10-01 15:46:00 -0400

    test client

--- lib_server/main/mylib/src/Message.cpp
+++ lib_server/main/mylib/src/Message.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 
 
+
 Message::Message(std::string username, int msg_type, int team, std::string msg){
     json d;
     if(username.size()>24 
@@ -23,3 +24,7 @@ Message::Message(std::string username, int msg_type, int team, std::string msg){
 int Message::getTeam(){
     return(data["team"]);
 }
+
+std::string Message::ToSend(){
+    return data.dump();
+} 
